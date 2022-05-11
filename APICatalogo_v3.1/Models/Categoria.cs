eab45746634c12aa84d5ba9bf9cd88a72aa33a20
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-05-10 21:40:00 -0300

    primeiro commit

---
+++ APICatalogo_v3.1/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo_v3._1.Models
+{
+    public class Categoria
+    {
+
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+
+    }
+}
