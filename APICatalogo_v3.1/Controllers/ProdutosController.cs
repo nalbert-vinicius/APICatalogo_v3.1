a883d28736625c5104e3edbee3fd15c77435afe0
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-05-14 12:45:00 -0300

    inicio controller

---
+++ APICatalogo_v3.1/Controllers/ProdutosController.cs
@@ -0,0 +1,31 @@
+﻿using APICatalogo_v3._1.Context;
+using APICatalogo_v3._1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo_v3._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.ToList();
+        }
+
+    }
+}
--- APICatalogo_v3.1/Models/Categoria.cs
+++ APICatalogo_v3.1/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace APICatalogo_v3._1.Models
 {
     public class Categoria
     {
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
 
         public int CategoriaId { get; set; }
         public string Nome { get; set; }
