63ac11db14a4a956d23b2bd8296aef364e83e973
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-05-16 20:58:13 -0300

    controller produto

--- APICatalogo_v3.1/Controllers/ProdutosController.cs
+++ APICatalogo_v3.1/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using APICatalogo_v3._1.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,8 +25,63 @@ namespace APICatalogo_v3._1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            return _context.Produtos.ToList();
+            return _context.Produtos.AsNoTracking().ToList();
         }
+
+        [HttpGet("{id}",Name = "GetById")]
+        public ActionResult<Produto> GetById(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public ActionResult<Produto> Update(Produto produto)
+        {
+            if(produto == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("GetById", new { produto.ProdutoId}, produto);
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult<Produto> Update(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Update(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok("Produto removido!");
+        }
+
+
 
     }
 }
