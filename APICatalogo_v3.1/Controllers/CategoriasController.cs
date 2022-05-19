cc37b7e97c5f30dc0ea16130d86e5818e5f60068
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-05-18 21:22:58 -0300

    categoriacontroller

---
+++ APICatalogo_v3.1/Controllers/CategoriasController.cs
@@ -0,0 +1,82 @@
+﻿using APICatalogo_v3._1.Context;
+using APICatalogo_v3._1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICatalogo_v3._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetByCategoriaId")]
+        public ActionResult<Produto> GetById(int id)
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult<Produto> Update(Categoria categoria)
+        {
+            if (categoria == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("GetByCategoriaId", new { categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult<Categoria> Update(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Update(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok("Categoria removida!");
+        }
+
+
+    }
+}
--- APICatalogo_v3.1/Startup.cs
+++ APICatalogo_v3.1/Startup.cs
@@ -1,18 +1,11 @@
 using APICatalogo_v3._1.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace APICatalogo_v3._1
 {
